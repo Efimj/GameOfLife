84caa7b548cacd3f774e1cf1eee34211e2291614
EFFemole <go.netgle@gmail.com>
2024-02-20 14:10:38 +0200

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.matrix"
+    namespace = "com.jobik.gameoflife"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.matrix"
+        applicationId = "com.jobik.gameoflife"
         minSdk = 27
         targetSdk = 34
         versionCode = 1
