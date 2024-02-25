a6c7d7155ae56f009e3d3d64b6c218f3d20a1b33
EFFemole <go.netgle@gmail.com>
2024-02-25 20:15:58 +0200

    added splash screen

--- app/src/main/java/com/jobik/gameoflife/ui/composables/OneGameUnit.kt
+++ app/src/main/java/com/jobik/gameoflife/ui/composables/OneGameUnit.kt
@@ -6,15 +6,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.jobik.gameoflife.gameOfLife.GameOfLife.Companion.GameOfLifeUnitState
 
 @Keep
