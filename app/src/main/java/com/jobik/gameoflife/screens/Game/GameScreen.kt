f9ac8cd8b975d53b82e2ff9b555ff269f8039a13
EFFemole <go.netgle@gmail.com>
2024-03-01 19:10:06 +0200

    update

--- app/src/main/java/com/jobik/gameoflife/screens/Game/GameScreen.kt
+++ app/src/main/java/com/jobik/gameoflife/screens/Game/GameScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.jobik.gameoflife.ui.helpers.horizontalWindowInsetsPadding
 import com.jobik.gameoflife.ui.helpers.topWindowInsetsPadding
-import com.jobik.gameoflife.ui.helpers.verticalWindowInsetsPadding
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
---
+++ app/src/main/java/com/jobik/gameoflife/screens/Settings/SettingsScreen.kt
@@ -0,0 +1,8 @@
+package com.jobik.gameoflife.screens.Settings
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsScreen(){
+
+}
