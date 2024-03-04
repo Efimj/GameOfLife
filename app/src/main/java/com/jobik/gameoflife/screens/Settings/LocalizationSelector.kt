4463b9dc4f9d2eab3702fefd641989745b23efc6
EFFemole <go.netgle@gmail.com>
2024-03-04 12:04:12 +0200

    update: refactoring

--- app/src/main/java/com/jobik/gameoflife/screens/Settings/LocalizationSelector.kt
+++ app/src/main/java/com/jobik/gameoflife/screens/Settings/LocalizationSelector.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.util.Log
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
