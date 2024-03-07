43ff5a4ac4a925c95fd8c9ba8dc1b896b9ca13dd
EFFemole <go.netgle@gmail.com>
2024-03-07 14:22:23 +0200

    update: theme selector

--- app/src/main/java/com/jobik/gameoflife/ui/theme/Palette.kt
+++ app/src/main/java/com/jobik/gameoflife/ui/theme/Palette.kt
@@ -8,11 +8,20 @@ import com.jobik.gameoflife.ui.theme.*
 enum class Palette(val light: ColorScheme, val dark: ColorScheme) {
     DynamicPalette(light = LightDefaultPalette, dark = DarkDefaultPalette),
     DefaultPalette(light = LightDefaultPalette, dark = DarkDefaultPalette),
-    OrangePalette(light = LightOrangePalette, dark = DarkOrangePalette),
-    AvocadoPalette(light = LightAvocadoPalette, dark = DarkAvocadoPalette),
-    LemonPalette(light = LightLemonPalette, dark = DarkLemonPalette),
-    SeaPalette(light = LightSeaPalette, dark = DarkSeaPalette),
-    GreenPalette(light = LightGreenPalette, dark = DarkGreenPalette);
+    BluePalette(light = LightBluePalette, dark = DarkBluePalette),
+    Blue2Palette(light = LightBlue2Palette, dark = DarkBlue2Palette),
+    GreenPalette(light = LightGreenPalette, dark = DarkGreenPalette),
+    Green2Palette(light = LightGreen2Palette, dark = DarkGreen2Palette),
+    YellowPalette(light = LightYellowPalette, dark = DarkYellowPalette),
+    Yellow2Palette(light = LightYellow2Palette, dark = DarkYellow2Palette),
+    PeachPalette(light = LightPeachPalette, dark = DarkPeachPalette),
+    Peach2Palette(light = LightPeach2Palette, dark = DarkPeach2Palette),
+    PurplePalette(light = LightPurplePalette, dark = DarkPurplePalette),
+    Purple2Palette(light = LightPurple2Palette, dark = DarkPurple2Palette),
+    NauticalPalette(light = LightNauticalPalette, dark = DarkNauticalPalette),
+    Nautical2Palette(light = LightNautical2Palette, dark = DarkNautical2Palette),
+    PinkPalette(light = LightPinkPalette, dark = DarkPinkPalette),
+    Pink2Palette(light = LightPink2Palette, dark = DarkPink2Palette);
 
     fun getPalette(isDarkTheme: Boolean): ColorScheme {
         return if (isDarkTheme) dark else light
