6290d81dcb531ee344e023b7843bd70f2a9f3fd6
EFFemole <go.netgle@gmail.com>
2024-03-08 22:16:19 +0200

    update: adaptive layout

--- app/src/main/java/com/jobik/gameoflife/screens/AppLayout/LayoutWithPermanentNavigationDrawer.kt
+++ app/src/main/java/com/jobik/gameoflife/screens/AppLayout/LayoutWithPermanentNavigationDrawer.kt
@@ -37,83 +37,72 @@ fun LayoutWithPermanentNavigationDrawer(
 
     PermanentNavigationDrawer(
         drawerContent = {
-            PermanentDrawerSheet(
-                modifier = Modifier
-                    .startWindowInsetsPadding()
-                    .width(240.dp),
-                windowInsets = WindowInsets.ime,
-                drawerContainerColor = MaterialTheme.colorScheme.surfaceContainer
-            ) {
-                Surface(color = MaterialTheme.colorScheme.surfaceContainer) {
+            Surface(color = MaterialTheme.colorScheme.surfaceContainer) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .verticalScroll(rememberScrollState())
+                        .startWindowInsetsPadding()
+                        .padding(horizontal = 20.dp)
+                        .widthIn(max = 240.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    TopWindowInsetsSpacer()
+                    Image(
+                        modifier = Modifier
+                            .size(150.dp)
+                            .padding(vertical = 20.dp),
+                        painter = painterResource(id = R.drawable.icon),
+                        contentDescription = "Main app icon"
+                    )
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 5.dp),
+                        text = stringResource(id = R.string.GameOfLife),
+                        color = MaterialTheme.colorScheme.onSurface,
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontWeight = FontWeight.SemiBold
+                    )
                     Column(
                         modifier = Modifier
-                            .fillMaxHeight()
-                            .verticalScroll(rememberScrollState())
-                            .startWindowInsetsPadding()
-                            .widthIn(max = 280.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(10.dp)
                     ) {
-                        TopWindowInsetsSpacer()
-                        Image(
-                            modifier = Modifier
-                                .size(150.dp)
-                                .padding(vertical = 20.dp),
-                            painter = painterResource(id = R.drawable.icon),
-                            contentDescription = "Main app icon"
-                        )
-                        Text(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 10.dp)
-                                .padding(bottom = 5.dp),
-                            text = stringResource(id = R.string.GameOfLife),
-                            color = MaterialTheme.colorScheme.onSurface,
-                            style = MaterialTheme.typography.headlineMedium,
-                            fontWeight = FontWeight.SemiBold
-                        )
-                        Column(
-                            modifier = Modifier
-                                .padding(horizontal = 10.dp)
-                        ) {
-                            DrawerParams.drawerButtons.forEach { button ->
-                                NavigationDrawerItem(
-                                    icon = { Icon(button.icon, contentDescription = null) },
-                                    label = { Text(text = stringResource(id = button.title)) },
-                                    selected = button.route.name == currentRoute,
-                                    colors = NavigationDrawerItemDefaults.colors(unselectedContainerColor = MaterialTheme.colorScheme.surfaceContainer),
-                                    onClick = {
-                                        coroutineScope.launch {
-                                            drawerState.close()
-                                        }
-                                        if (button.route.name == currentRoute) return@NavigationDrawerItem
-                                        if (navController.canNavigate().not()) return@NavigationDrawerItem
-                                        navController.navigate(button.route.name) {
-                                            // pops the route to root and places new screen
-                                            popUpTo(button.route.name)
-                                        }
-                                    },
-                                    modifier = Modifier.padding(horizontal = 12.dp)
-                                )
-                            }
-                            BottomWindowInsetsSpacer()
-                            Spacer(modifier = Modifier.weight(1f, fill = true))
-                            Row(
-                                modifier = Modifier
-                                    .padding(bottom = 5.dp)
-                                    .padding(horizontal = 10.dp),
-                                verticalAlignment = Alignment.Bottom,
-                                horizontalArrangement = Arrangement.Center
-                            ) {
-                                Text(
-                                    text = stringResource(id = R.string.app_version_text) + BuildConfig.VERSION_NAME,
-                                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = .7f),
-                                    style = MaterialTheme.typography.labelMedium
-                                )
-                            }
-                            BottomWindowInsetsSpacer()
+                        DrawerParams.drawerButtons.forEach { button ->
+                            NavigationDrawerItem(
+                                icon = { Icon(button.icon, contentDescription = null) },
+                                label = { Text(text = stringResource(id = button.title)) },
+                                selected = button.route.name == currentRoute,
+                                colors = NavigationDrawerItemDefaults.colors(unselectedContainerColor = MaterialTheme.colorScheme.surfaceContainer),
+                                onClick = {
+                                    coroutineScope.launch {
+                                        drawerState.close()
+                                    }
+                                    if (button.route.name == currentRoute) return@NavigationDrawerItem
+                                    if (navController.canNavigate().not()) return@NavigationDrawerItem
+                                    navController.navigate(button.route.name) {
+                                        // pops the route to root and places new screen
+                                        popUpTo(button.route.name)
+                                    }
+                                },
+                            )
                         }
                     }
+                    BottomWindowInsetsSpacer()
+                    Spacer(modifier = Modifier.weight(1f, fill = true))
+                    Row(
+                        modifier = Modifier
+                            .padding(bottom = 5.dp),
+                        verticalAlignment = Alignment.Bottom,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.app_version_text) + BuildConfig.VERSION_NAME,
+                            color = MaterialTheme.colorScheme.onSurface.copy(alpha = .7f),
+                            style = MaterialTheme.typography.labelMedium
+                        )
+                    }
+                    BottomWindowInsetsSpacer()
                 }
             }
         },
@@ -121,7 +110,8 @@ fun LayoutWithPermanentNavigationDrawer(
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(20.dp)
+                    .padding(vertical = 20.dp)
+                    .padding(end = 20.dp)
             ) {
                 AppNavHost(
                     navController = navController,
