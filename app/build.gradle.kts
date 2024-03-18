2a21a720b944f0b74c123105f029e52f38d559ee
EFFemole <go.netgle@gmail.com>
2024-03-18 19:48:49 +0200

    gradle version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 27
         targetSdk = 34
         versionCode = 1
-        versionName = "0.4.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
